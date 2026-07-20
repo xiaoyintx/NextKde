0f5b420e5f0b20fb5dfba02084e9ae315f9b22b8
winter_l <hainan_winter_l@outlook.com>
2026-07-21 00:58:27 +0800

    Switch from zed-editor to zed-editor-fhs
    Drop the old zed-editor configuration block including
    extensions and extraPackages

--- software/zed-editor.nix
+++ software/zed-editor.nix
@@ -4,18 +4,7 @@
   # 使用 Home Manager 安装 zed editor
   home-manager.users.winterl = { config, pkgs, ... }: {
     home.packages = with pkgs; [
-      zed-editor
+      zed-editor-fhs
     ];
-    programs.zed-editor = {
-      extensions = [
-        "nix"
-        "colored-zed-icons-theme"
-        "fleet-themes"
-        "git-firefly"
-      ];
-      extraPackages = with pkgs; [
-        nixd
-      ];
-    };
   };
 }
