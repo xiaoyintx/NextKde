46df763c5d95382230b85dd1d58c980aa8494bdc
winter_l <hainan_winter_l@outlook.com>
2026-08-09 01:33:49 +0800

    kvm 安全启动配置

--- hosts/rog-m16/kvm.nix
+++ hosts/rog-m16/kvm.nix
@@ -1,3 +1,4 @@
+{ lib, pkgs, ... }:
 let
   # 设备的PCI ID
   # IOMMU Group 8:
@@ -7,8 +8,15 @@ let
     "10de:2520" # Graphics
     "10de:228e" # Audio
   ];
+
+  # 带 Secure Boot 支持和 Microsoft 预置密钥的 OVMF 固件。
+  # 新版 nixpkgs 不再提供 virtualisation.efi 或 libvirtd.qemu.ovmf 选项，
+  # 因此将固件暴露在稳定路径下，由虚拟机 XML 直接引用。
+  ovmfMs = (pkgs.OVMF.override {
+    secureBoot = true;
+    msVarsTemplate = true;
+  }).fd;
 in
-{ lib, pkgs, ... }:
 {
   specialisation."GPUPaththrough".configuration = {
     system.nixos.tags = [
@@ -28,12 +36,18 @@ in
     ];
   };
 
+  # 给 libvirt 提供不会包含 Nix store 哈希的固定固件路径。
+  systemd.tmpfiles.rules = [
+    "L+ /run/ovmf-ms - - - - ${ovmfMs}"
+  ];
+
   # KVM
   virtualisation = {
     spiceUSBRedirection.enable = true;
     libvirtd = {
       enable = true;
       qemu = {
+        runAsRoot = true;
         swtpm.enable = true;
       };
     };
