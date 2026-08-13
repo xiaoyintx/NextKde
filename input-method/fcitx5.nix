c1bd8f3686e8fdff378ac80de2f92885048252e3
xiaoyintx <yinyue3260669043@outlook.com>
2026-08-13 18:44:03 +0800

    input method?

--- input-method/fcitx5.nix
+++ input-method/fcitx5.nix
@@ -2,9 +2,9 @@
 
 let
   # 1. 覆盖 fcitx5-rime，指定使用 rime-ice 作为数据源
-  wanxiang = pkgs.fcitx5-rime.override {
+  ice = pkgs.fcitx5-rime.override {
     rimeDataPkgs = with pkgs; [
-      rime-wanxiang # 雾凇拼音方案
+      rime-ice # 雾凇拼音方案
     ];
   };
 in
@@ -15,7 +15,7 @@ in
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
-        wanxiang # 使用我们定制后的 rime 包
+        ice # 使用我们定制后的 rime 包
         fcitx5-gtk
         kdePackages.fcitx5-qt
       ];
