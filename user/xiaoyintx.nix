2c00c31d68b0a0fe8e5d67783ef62b0a8dbc328b
xiaoyintx <yinyue3260669043@outlook.com>
2026-08-30 10:33:41 +0800

    nothing?

--- user/xiaoyintx.nix
+++ user/xiaoyintx.nix
@@ -136,12 +136,5 @@
                 '';
                 force = true;
             };
-            xdg.dataFile = {
-                # v2rayn
-                "v2rayN/bin/sing_box/sing-box".source = "${pkgs.sing-box}/bin/sing-box";
-                "v2rayN/bin/xray/xray".source = "${pkgs.xray}/bin/xray";
-                "v2rayN/bin/geoip.dat".source = "${pkgs.v2ray-geoip}/share/v2ray/geoip.dat";
-                "v2rayN/bin/geosite.dat".source = "${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat";
-            };
         };
 }
